8839d9bb59bc88535d5ab16caa8879b12ab344a5
HP <triavr@mail.ru>
2022-04-25 15:50:51 +0400

    com2

---
+++ src/Cat.java
@@ -0,0 +1,19 @@
+public class Cat {
+
+    private  String name;
+    private String color:
+
+    public String getName() {
+        return name;
+
+    }
+    public void setName(String a) {
+        name = a;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor() {
+        this.color = color;
+    }
+}
